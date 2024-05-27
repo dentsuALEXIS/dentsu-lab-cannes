b5d9d37f216c563afa31d8dddc93c632685c53cf
dentsuALEXIS <alexis.toromanoff@dentsuachtung.com>
2024-05-27 16:46:22 +0200

    Initial Storyblok setup

---
+++ src/components/chapters/intro.svelte
@@ -0,0 +1,25 @@
+<script>
+	import { onMount } from "svelte";
+	import { getStoryblokApi } from "@storyblok/svelte";
+
+	let story = null;
+	onMount(async () => {
+		const storyblokApi = getStoryblokApi();
+
+		const { data } = await storyblokApi.get("cdn/stories/home", {
+			version: "draft",
+		});
+
+		story = data.story;
+	});
+</script>
+
+<section
+	class="fixed inset-0 flex h-full items-center justify-center text-white"
+>
+	<span>Intro</span>
+</section>
+
+<style>
+	
+</style>
