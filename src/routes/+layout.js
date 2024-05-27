a02ea735380d66fd3a6f115daee735051fef7847
dentsuALEXIS <alexis.toromanoff@dentsuachtung.com>
2024-05-27 16:46:22 +0200

    Initial Storyblok setup

---
+++ src/routes/+layout.js
@@ -0,0 +1,15 @@
+import { storyblokInit, apiPlugin } from "@storyblok/svelte";
+import { PUBLIC_STORYBLOK_ACCESS_TOKEN_PREVIEW } from "$env/static/public"
+
+// https://www.storyblok.com/tp/add-a-headless-cms-to-svelte-in-5-minutes
+export async function load() {
+	storyblokInit({
+		accessToken: PUBLIC_STORYBLOK_ACCESS_TOKEN_PREVIEW,
+		// bridge: false,
+		use: [apiPlugin],
+		apiOptions: {
+			https: true,
+		},
+	});
+}
+
