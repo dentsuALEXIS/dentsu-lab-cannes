ccaf9faf1478d7021a18ca852887079ed355c08b
dentsuALEXIS <alexis.toromanoff@dentsuachtung.com>
2024-05-28 09:59:23 +0200

    Add basic ssl

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { sveltekit } from '@sveltejs/kit/vite'
 import { defineConfig } from 'vitest/config'
-// import basicSsl from "@vitejs/plugin-basic-ssl";
 import mkcert from 'vite-plugin-mkcert'
 
 export default defineConfig({
